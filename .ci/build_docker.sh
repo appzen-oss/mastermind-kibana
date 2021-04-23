4e402d6fa91b3313a197b0f5e5c853d2be5d1a11
Damian Bocanegra <damian@appzen>
2021-04-22 18:45:25 -0700

    updates build_docker.sh

--- .ci/build_docker.sh
+++ .ci/build_docker.sh
@@ -8,7 +8,7 @@ cp /usr/local/bin/runbld ./
 cp /usr/local/bin/bash_standard_lib.sh ./
 
 if which docker >/dev/null; then
-    docker build -t kibana-ci -f ./Dockerfile .
+    def customImage = docker.build("kibana-ci", "-f ./Dockerfile")
 else
     echo "Docker binary is not available. Skipping the docker build this time."
 fi
