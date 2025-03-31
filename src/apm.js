95a48f3e6e8bea0ac4a31bfbbdfd786a63213eae
Ramanuj Shastri <ramanuj.shastri@appzen.com>
2025-03-31 15:57:37 +0530

    install dd agent for mastermind-ui

--- src/apm.js
+++ src/apm.js
@@ -17,47 +17,17 @@
  * under the License.
  */
 
-const { join } = require('path');
-const { name, build } = require('../package.json');
-const { loadConfiguration } = require('@kbn/apm-config-loader');
-
-const ROOT_DIR = join(__dirname, '..');
-let apmConfig;
-
-/**
- * Flag to disable APM RUM support on all kibana builds by default
- */
-const isKibanaDistributable = Boolean(build && build.distributable === true);
-
-module.exports = function (serviceName = name) {
-  apmConfig = loadConfiguration(process.argv, ROOT_DIR, isKibanaDistributable);
-  const conf = apmConfig.getConfig(serviceName);
-  const apm = require('elastic-apm-node');
-
-  // Filter out all user PII
-  apm.addFilter((payload) => {
-    try {
-      if (payload.context && payload.context.user && typeof payload.context.user === 'object') {
-        Object.keys(payload.context.user).forEach((key) => {
-          payload.context.user[key] = '[REDACTED]';
-        });
-      }
-    } finally {
-      return payload;
-    }
-  });
-
-  apm.start(conf);
-};
-
-module.exports.getConfig = (serviceName) => {
-  // integration test runner starts a kibana server that import the module without initializing APM.
-  // so we need to check initialization of the config.
-  // note that we can't just load the configuration during this module's import
-  // because jest IT are ran with `--config path-to-jest-config.js` which conflicts with the CLI's `config` arg
-  // causing the config loader to try to load the jest js config as yaml and throws.
-  if (apmConfig) {
-    return apmConfig.getConfig(serviceName);
+const tracer = require('dd-trace').init({
+  service: process.env.DD_SERVICE || 'kibana',
+  env: process.env.NODE_ENV || 'enft',
+  logInjection: true,
+  runtimeMetrics: true,
+  profiling: true,
+  // Configure the agent to send traces to the Datadog cluster agent
+  agent: {
+    host: process.env.DD_AGENT_HOST || 'localhost',
+    port: process.env.DD_TRACE_AGENT_PORT || 8126
   }
-  return {};
-};
+});
+
+module.exports = tracer;
