f6857263d66116efc18995be731a708bf97ab924
Raunak Thakkar <raunak.thakkar@appzen.com>
2025-04-17 13:08:39 +0530

    AENG-1236 update: updated appzen favicon for mm-apps

--- src/core/server/rendering/views/template.tsx
+++ src/core/server/rendering/views/template.tsx
@@ -79,18 +79,6 @@ export const Template: FunctionComponent<Props> = ({
           sizes="180x180"
           href={`${uiPublicUrl}/favicons/apple-touch-icon.png`}
         />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href={`${uiPublicUrl}/favicons/favicon-32x32.png`}
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href={`${uiPublicUrl}/favicons/favicon-16x16.png`}
-        />
         <link rel="manifest" href={`${uiPublicUrl}/favicons/manifest.json`} />
         <link
           rel="mask-icon"
