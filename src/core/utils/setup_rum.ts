a35e22e18c04a930ad76bbfec94367ab785c6fb5
Gaurav Kumar <gaurav.kumar@appzen.com>
2026-02-02 14:12:20 +0530

    Add datadog rum

---
+++ src/core/utils/setup_rum.ts
@@ -0,0 +1,52 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import { datadogRum } from '@datadog/browser-rum';
+
+interface DatadogRumConfig {
+  applicationId?: string;
+  clientToken?: string;
+  site?: string;
+  enabled?: boolean;
+}
+
+export function initializeDatadogRUM(rumConfig?: DatadogRumConfig) {
+  if (!rumConfig?.enabled || !rumConfig?.applicationId || !rumConfig?.clientToken) {
+    return;
+  }
+
+  datadogRum.init({
+    applicationId: rumConfig.applicationId,
+    clientToken: rumConfig.clientToken,
+    site: rumConfig.site || 'datadoghq.com',
+    service: 'kibana',
+    env: process.env.NODE_ENV || 'development',
+    version: '8.0.0',
+    sessionSampleRate: 100,
+    sessionReplaySampleRate: 20,
+    trackUserInteractions: true,
+    trackResources: true,
+    trackLongTasks: true,
+    defaultPrivacyLevel: 'mask-user-input',
+    allowedTracingUrls: [{ match: /https:\/\/.*\.appzen\.com/, propagatorTypes: ['datadog'] }],
+    enableExperimentalFeatures: ['feature_flags'],
+  });
+
+  datadogRum.startSessionReplayRecording();
+}
